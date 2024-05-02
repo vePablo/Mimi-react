c9db6bc80703aa8fca7353edcf1dba7423fde547
vePablo <vepablo88@gmail.com>
2024-05-01 23:36:38 -0300

    env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
+const firebaseConfig = {
+  apiKey: "AIzaSyCCsNmnn33u6pCNBj8VkGH-Ul74_j1PFYc",
+  authDomain: "soy-mimi.firebaseapp.com",
+  projectId: "soy-mimi",
+  storageBucket: "soy-mimi.appspot.com",
+  messagingSenderId: "239508614169",
+  appId: "1:239508614169:web:aa6d65f3c4ee6ef5f00e88"
+};
+//const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
